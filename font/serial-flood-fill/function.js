ba8dd947d7dfd988e4156099788a9e00d1f7b97d
Nico <jnico.chilla@gmail.com>
2021-10-20 03:09:25 -0400

    flood-fill and other tests

---
+++ font/serial-flood-fill/function.js
@@ -0,0 +1,286 @@
+
+
+var resolutionXy={x:500,y:500};
+let canvas=document.querySelector('canvas');
+let ctx=canvas.getContext('2d');
+let squares=[];
+let sq=20;
+let xCount=resolutionXy.x/sq;
+let yCount=resolutionXy.y/sq;
+
+// let mouse
+
+
+document.querySelectorAll('button').forEach((item, i) => {
+  item.addEventListener('click',function(){
+    let target=event.currentTarget;
+    switch(target.id){
+      case 'brute-force':
+      bruteForce();
+      break;
+      case 'brute-force-voronoi':
+      bruteForceVoronoi();
+      break;
+      case 'flood-fill':
+      floodGradient();
+      break;
+      case 'flood-fill-voronoi':
+      floodFillVoronoi();
+      break;
+
+    }
+  })
+});
+
+
+
+
+
+let textured=[
+  {x:5,y:10,i:0,k:'a'},
+  {x:20,y:23,i:0,k:'b'},
+  {x:17,y:10,i:0,k:'c'},
+  {x:9,y:5,i:0,k:'d'},
+
+
+]
+
+
+
+window.addEventListener('resize',resolutionChange);
+window.addEventListener('load',startUp);
+
+
+
+function startUp(){
+  resolutionChange();
+  setUpSquares();
+  draw();
+  // bruteForce();
+  // bruteForceVoronoi();
+  // floodGradient();
+
+}
+
+function resolutionChange(){
+  canvas.width=canvas.offsetWidth;
+  canvas.height=canvas.offsetHeight;
+
+  resolutionXy.x=canvas.width;
+  resolutionXy.y=canvas.height;
+
+  setUpSquares();
+  draw();
+
+}
+
+function setUpSquares(){
+  squares=[];
+  xCount=resolutionXy.x/sq;
+  yCount=resolutionXy.y/sq;
+  for(let x=0;x<xCount;x++){
+    for(let y=0;y<yCount;y++){
+      squares.push({x:x,y:y});
+    }
+  }
+  // console.log(squares);
+}
+
+
+
+function fillSquare(vector,color){
+  ctx.fillStyle=color;
+  ctx.fillRect(vector.x*sq,vector.y*sq,sq,sq);
+}
+
+
+function floodGradient(){
+  let flood=[...squares];
+  let filled=[...textured];
+  let layer=1;
+
+  fillLayer();
+
+  function fillLayer(){
+    let changeCount=0;
+    filled.forEach((item, i) => {
+      //fill texture square
+      let col=item.i*10;
+      fillSquare(item,`rgb(${col},${col},${col})`);
+      let neighbors=getNeighbors(item,{x:xCount,y:yCount},layer);
+      for(var x=0;x<4;x++){
+        let neigh=neighbors[x];
+        if(!filled.some(a=>a.x==neigh.x&&a.y==neigh.y)){
+          filled.push(neigh);
+          let ncol=neigh.i*10;
+          fillSquare(neigh,`rgb(${ncol},${ncol},${ncol})`);
+
+          changeCount++;
+        }
+      }
+
+    });
+    // console.log(changeCount);
+    if(changeCount>0){
+
+      layer++;
+      window.requestAnimationFrame(fillLayer);
+
+    }
+
+  }
+
+
+}
+
+function floodFillVoronoi(){
+  let flood=[...squares];
+  let filled=[...textured];
+  let layer=1;
+
+  fillLayer();
+
+  function decideColor(val){
+    switch(val){
+      case 'a':
+      return 'green';
+      break;
+      case 'b':
+      return 'blue';
+      break;
+      case 'c':
+      return 'red';
+      break;
+      case 'd':
+      return 'yellow';
+      break;
+    }
+  }
+
+  function fillLayer(){
+    let changeCount=0;
+    filled.forEach((item, i) => {
+      //fill texture square
+      let col=item.i*10;
+
+      fillSquare(item,decideColor(item.k));
+      let neighbors=getNeighbors(item,{x:xCount,y:yCount},layer,item.k);
+      for(var x=0;x<4;x++){
+        let neigh=neighbors[x];
+        if(!filled.some(a=>a.x==neigh.x&&a.y==neigh.y)){
+          filled.push(neigh);
+          let ncol=neigh.i*10;
+
+          fillSquare(neigh,decideColor(item.k));
+
+          changeCount++;
+        }
+      }
+
+    });
+    // console.log(changeCount);
+    if(changeCount>0){
+
+      layer++;
+      window.requestAnimationFrame(fillLayer);
+
+    }
+}
+
+}
+
+function getNeighbors(v,bounds,layer,color){
+  let vals=[
+    {x:v.x-1,y:v.y,i:layer,k:color},
+    {x:v.x+1,y:v.y,i:layer,k:color},
+    {x:v.x,y:v.y-1,i:layer,k:color},
+    {x:v.x,y:v.y+1,i:layer,k:color},
+    {x:v.x+1,y:v.y+1,i:layer,k:color},
+    {x:v.x+1,y:v.y-1,i:layer,k:color},
+    {x:v.x-1,y:v.y+1,i:layer,k:color},
+    {x:v.x-1,y:v.y-1,i:layer,k:color}
+
+  ]
+  for(var i=0;i<4;i++){
+    if(
+      vals[i].x<0||vals[i].x>bounds.x||
+      vals[i].y<0||vals[i].y>bounds.y
+    ){
+      vals[i]=v;
+    }
+  }
+  return vals;
+}
+
+
+
+function draw(){
+
+  for(let i=0;i<squares.length;i++){
+    if(textured.some(a=>a.x==squares[i].x&&a.y==squares[i].y)){
+      ctx.fillStyle='rgb(0,0,0)';
+    }else{
+      ctx.fillStyle=`rgb(255,255,255)`;
+    }
+    ctx.fillRect(squares[i].x*sq,squares[i].y*sq,sq,sq);
+  }
+  // console.log(pos);
+
+}
+
+
+function distance(vec1,vec2){
+  return Math.sqrt(Math.pow(vec2.x-vec1.x,2)+Math.pow(vec2.y-vec1.y,2));
+}
+
+
+function bruteForce(){
+  for(let s=0;s<squares.length;s++){
+    let allPos=[];
+    for(let t=0;t<textured.length;t++){
+      allPos.push(distance(textured[t],squares[s]));
+    }
+    let pos=Math.min(...allPos);
+    pos=pos*10;
+    ctx.fillStyle=`rgb(${pos},${pos},${pos})`;
+    ctx.fillRect(squares[s].x*sq,squares[s].y*sq,sq,sq);
+  }
+
+}
+
+function bruteForceVoronoi(){
+  for(let s=0;s<squares.length;s++){
+    let dist=2000;
+    let ind=-1;
+    for(let t=0;t<textured.length;t++){
+      let newDist=distance(textured[t],squares[s]);
+      if(newDist<dist){
+        ind=t;
+        dist=newDist;
+      }
+
+      distance(textured[t],squares[s])<dist
+    }
+    let fill;
+    switch(ind){
+      case 0:
+        fill="green";
+      break;
+      case 1:
+        fill="blue";
+      break;
+      case 2:
+        fill="red";
+      break;
+      case 3:
+        fill="yellow";
+      break;
+      default:
+        fill="white";
+    }
+
+    ctx.fillStyle=fill;
+    ctx.fillRect(squares[s].x*sq,squares[s].y*sq,sq,sq);
+  }
+
+}
---
+++ ray-marching/march.frag
@@ -0,0 +1,91 @@
+precision lowp float;
+
+const int steps =  64;
+const float smallNumber=0.001;
+const float maxDist=10.;
+
+uniform float u_time;
+uniform vec2 u_resolution;
+
+float scene(vec3 position){
+    vec3 editPos=vec3(position.x+ sin(u_time),position.y + 1.2,position.z);
+    float sphere=length(editPos) - 01.;
+    float ground=position.y + sin(position.x*5.)/5. + 3.
+        +cos(position.z * 5.)/5.;
+    // float ground=position.y + sin(position.x*5.)/2. + 3.
+    // +cos(position.z * 5.)/1.;
+
+
+    // return sphere;
+
+    return min(ground,sphere);
+}
+
+
+ vec3 cosPalette( float t , vec3 brightness, vec3 contrast, vec3 osc, vec3 phase){
+    return brightness + contrast*cos( 6.28318*(osc*t+phase) );
+}
+
+vec3 estimateNormal(vec3 p) {
+    // define your own small number here or at the top
+    // im just not doing it here because in my workshop
+    // i have it as a const global.
+    vec3 n = vec3(
+        scene(vec3(p.x + smallNumber, p.yz)) -
+        scene(vec3(p.x - smallNumber, p.yz)),
+        scene(vec3(p.x, p.y + smallNumber, p.z)) -
+        scene(vec3(p.x, p.y - smallNumber, p.z)),
+        scene(vec3(p.xy, p.z + smallNumber)) -
+        scene(vec3(p.xy, p.z - smallNumber))
+    );
+    return normalize(n);
+}
+
+
+vec4 march(vec3 origin, vec3 dir){
+    float dist = 0.;
+    float totalDist=0.;
+    vec3 positionOnRay=origin;
+
+    for(int i =0; i < steps ;i++){
+
+        dist=scene(positionOnRay);
+        // dist=length(positionOnRay) - 01.5;
+        positionOnRay += dist*dir;
+        totalDist+=dist;
+
+        if(dist < smallNumber){
+            vec3 brightness=vec3(0.3,0.3,0.3);
+            vec3 contrast=vec3(0.4,0.4,0.5);
+            vec3 phase=vec3(0.3,0.7,0.4);
+            vec3 osc=vec3(0.2,0.2,0.2);
+            vec3 color = cosPalette(positionOnRay.z/10.+u_time, brightness,contrast,osc,phase);
+            // vec3 color=estimateNormal(positionOnRay);
+            vec3 norm=estimateNormal(positionOnRay);
+            return vec4(color*norm,1.);
+            // return vec4(1.-totalDist/maxDist);
+        }
+    }
+    return vec4(0.);
+}
+
+
+
+
+void main () {
+    vec2 pos = gl_FragCoord.xy/u_resolution.x;
+
+    pos=(pos-0.5)*2.;
+    //  pox.x=pos.x * u_resolution.x/u_resolution.y;
+    // pos.y=pos.y + 0.25;
+    vec3 camOrigin=vec3(0.,0.,-5.);
+    vec3 rayOrigin=vec3(pos + camOrigin.xy,camOrigin.z + 1.);
+    vec3 dir=normalize(rayOrigin - camOrigin);
+
+
+
+    vec4 color=vec4(vec3(march(rayOrigin, dir)),1.);
+
+    gl_FragColor=color;
+// 	gl_FragColor = vec4(abs(pos),1., 1.0);
+}
